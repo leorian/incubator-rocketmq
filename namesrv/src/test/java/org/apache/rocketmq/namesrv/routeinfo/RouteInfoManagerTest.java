234ddc8bd97fab5659b61ce396c064ab6afff22b
xiezg <xiezg@317hu.com>
2017-06-01 16:53:05 +0800

    rocketmq配置

--- namesrv/src/test/java/org/apache/rocketmq/namesrv/routeinfo/RouteInfoManagerTest.java
+++ namesrv/src/test/java/org/apache/rocketmq/namesrv/routeinfo/RouteInfoManagerTest.java
@@ -44,7 +44,8 @@ public class RouteInfoManagerTest {
     @After
     public void terminate() {
         routeInfoManager.printAllPeriodically();
-        routeInfoManager.unregisterBroker("default-cluster", "127.0.0.1:10911", "default-broker", 1234);
+        routeInfoManager.unregisterBroker("default-cluster",
+                "127.0.0.1:10911", "default-broker", 1234);
     }
 
     @Test
@@ -73,7 +74,8 @@ public class RouteInfoManagerTest {
         topicConfigConcurrentHashMap.put("unit-test", topicConfig);
         topicConfigSerializeWrapper.setTopicConfigTable(topicConfigConcurrentHashMap);
         Channel channel = mock(Channel.class);
-        RegisterBrokerResult registerBrokerResult = routeInfoManager.registerBroker("default-cluster", "127.0.0.1:10911", "default-broker", 1234, "127.0.0.1:1001",
+        RegisterBrokerResult registerBrokerResult = routeInfoManager.registerBroker("default-cluster",
+                "127.0.0.1:10911", "default-broker", 1234, "127.0.0.1:1001",
             topicConfigSerializeWrapper, new ArrayList<String>(), channel);
         assertThat(registerBrokerResult).isNotNull();
     }
